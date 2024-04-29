8249f5f71f120047dc8692adc7b4b6c42ddf8cc3
Villa BORGHESE <rnjyomo@gmail.com>
2024-04-29 22:50:46 +0200

    Implemented authentication with flask and restricted resources on the website

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -7,6 +8,10 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
+
+# Flask login init
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 # CREATE DATABASE
@@ -20,7 +25,7 @@ db.init_app(app)
 
 
 # CREATE TABLE IN DB
-class User(db.Model):
+class User(db.Model, UserMixin):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
@@ -31,6 +36,12 @@ with app.app_context():
     db.create_all()
 
 
+# User loader
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -39,37 +50,60 @@ def home():
 @app.route('/register', methods=["POST", "GET"])
 def register():
     if request.method == "POST":
-        
         new_user = User(
             name=request.form.get("name"),
             email=request.form.get("email"),
-            password=request.form.get("password"),
+            password=generate_password_hash(request.form.get("password"), method="pbkdf2:sha256", salt_length=8),
         )
         db.session.add(new_user)
         db.session.commit()
+
+        # Login and validate the user
+        login_user(new_user)
+
+        flash('Logged in successfully.')
 
         return redirect(url_for("secrets"))
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        # get the user from the db
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+
+        if check_password_hash(user.password, password=password):
+            # log the user
+            login_user(user)
+            flash('Logged in successfully.')
+            return redirect(url_for("secrets", name=current_user.name))
+
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    print(current_user.name)
+    return render_template("secrets.html", name=current_user.name)
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for("home"))
 
 
 @app.route('/download')
+@login_required
 def download():
-    pass
+    return send_from_directory("static/files", "cheat_sheet.pdf")
 
 
 if __name__ == "__main__":
